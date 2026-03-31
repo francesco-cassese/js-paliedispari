f54ed020864581c95ca90abec44740eb2339b427
Francesco Cassese <francescocassese9@gmail.com>
2026-03-31 16:05:40 +0200

    Aggiunta richiesta user, richiamo funzione e consol log allo script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,14 @@
 'use strict';
+
+//================================================================
+//PALINDROMO
+//================================================================
+
+//CHIEDERE PAROLA A UTENTE
+const inputUser = prompt("Scrivi qui la parola/numero da valutare")
+
+//SALVO & RICHIAMO LA FUNZIONE
+const outcomeCheckPalindrome = checkPalindrome(inputUser);
+
+//STAMPO TUTTO
+console.log(outcomeCheckPalindrome);
