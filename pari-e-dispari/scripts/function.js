f568bfe6e946ab4a3848a6ca266acbe6836f4b63
Francesco Cassese <francescocassese9@gmail.com>
2026-03-31 16:41:33 +0200

    Aggiunta funzione per generare numero automatico

--- pari-e-dispari/scripts/function.js
+++ pari-e-dispari/scripts/function.js
@@ -1,1 +1,11 @@
 'use strict';
+
+//====================================================
+//GENERATOTRE NUMERI CASUALI
+//====================================================
+
+//CREO UNA FUNZIONE CHE MI GENERI I NUMERI AUTOMATICAMENTE
+function generatorRandomNumber(min, max) {
+    //VALORE MASSIMO es"5" - VALORE MINIMO es"1" + 1 PER COPRIRE L'INTERO RANGE E LO SOMMO DI NUOVO PER FAR ARRIVARE IL CALCOLO IN QUEL RANGE.
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
